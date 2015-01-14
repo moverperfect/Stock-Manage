bc2287fe00d021ceed52b904a2ffd5c358e12f50
Jack Moorhouse <moverperfect@gmail.com>
2015-01-14 16:50:46 +0000

    Added support for the event handler inside the login authentication

--- Stock Manage Client/Forms/Authentication.cs
+++ Stock Manage Client/Forms/Authentication.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using Stock_Manage_Client.Classes;
+using Stock_Manage_Client.Classes.Networking;
+using Stock_Manage_Client.Classes.Networking.Packets;
 
 namespace Stock_Manage_Client.Forms
 {
@@ -29,37 +30,25 @@ namespace Stock_Manage_Client.Forms
 
             Program.SendData("SELECT Salt,Password_Hash FROM tbl_Users WHERE PK_UserId=" + txt_UserId.Text + ";");
 
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-            while (stopwatch.ElapsedMilliseconds < 3000)
-            {
-                try
-                {
-                    if (Program.TempReturnTable.TableData.Rows.Count > 0)
-                    {
-                        var password = Utilities.HashPassword(txt_Psswrd.Text,
-                            Program.TempReturnTable.TableData.Rows[0]["Salt"].ToString(), MD5.Create());
-                        if (password == Program.TempReturnTable.TableData.Rows[0]["Password_Hash"].ToString())
-                        {
-                            Program.UserId = txt_UserId.Text;
-                            Close();
-                            return;
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                }
-            }
+            PacketHandler.DataRecieved += AuthenticatePassword;
+        }
 
-            if (Program.TempReturnTable != null)
+        private void AuthenticatePassword(byte[] packet)
+        {
+            var table = new Table(packet);
+            if (table.TableData.Rows.Count > 0)
             {
-                MessageBox.Show("Login failed, username or password is incorrect");
+                var password = Utilities.HashPassword(txt_Psswrd.Text,
+                    table.TableData.Rows[0]["Salt"].ToString(), MD5.Create());
+                if (password == table.TableData.Rows[0]["Password_Hash"].ToString())
+                {
+                    Program.UserId = txt_UserId.Text;
+                    PacketHandler.DataRecieved -= AuthenticatePassword;
+                    Invoke((MethodInvoker) Close);
+                    return;
+                }
             }
-            else
-            {
-                MessageBox.Show("Timeout to the server, please try again");
-            }
+            MessageBox.Show("Login failed, username or password is incorrect");
         }
     }
 }
