d096229d59c8417cc8af5008c1f060d9a1510adc
moverperfect <moverperfect@gmail.com>
2015-09-22 11:12:54 +0100

    Added the server side to the order notifications

--- Stock Manage Server/Networking/PacketHandler.cs
+++ Stock Manage Server/Networking/PacketHandler.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
-using System.Security.Cryptography.X509Certificates;
 using Stock_Manage_Client.Classes.Networking.Packets;
 
 namespace Stock_Manage_Server.Networking
@@ -13,6 +13,8 @@ namespace Stock_Manage_Server.Networking
     internal static class PacketHandler
     {
         internal static List<ProductNotification> LowProducts = new List<ProductNotification>();
+
+        internal static List<ProductNotification> VerifyOrders = new List<ProductNotification>(); 
 
         /// <summary>
         /// Handles all incoming packets into the server after fully recieving them
@@ -36,6 +38,8 @@ namespace Stock_Manage_Server.Networking
             // 2002 - Select
             // 2003 - Count
             // 2004 - Notification check
+            // 2005 - Low product notification
+            // 2006 - Verify Order Notification
             switch (packetType)
             {
                 case 2000:
@@ -73,7 +77,7 @@ namespace Stock_Manage_Server.Networking
                     break;
 
                 case 2004:
-                    // TODO Implement this to notify management of low products and of orders that need to be verified
+                    // TODO Refactor this to be in a seperate class and even combine the product and order to one single function
                     var check = new StdData(packet);
                     connecter = new SqlConnecter("db_inventorymanagement");
                     var isManagement =
@@ -129,10 +133,75 @@ namespace Stock_Manage_Server.Networking
                             LowProducts[index].ProductId.Add((int)sendProducts.Rows[i][0]);
                         }
 
-                        // Send the products back to the client
-                        var table = new Table(sendProducts,Program.MachineId,Program.UserId,2004);
-                        clientSocket.Send(table.Data);
-                        break;
+                        // If there are still products to send back to the client then send
+                        if (sendProducts.Rows.Count != 0)
+                        {
+                            // Send the products back to the client
+                            var table = new Table(sendProducts, Program.MachineId, Program.UserId, 2005);
+                            clientSocket.Send(table.Data);
+                            break;
+                        }
+
+                        // Grab the orders that need to be verified
+                        var approveOrders =
+                            (DataTable)
+                                connecter.Select(
+                                    "SELECT PK_OrderId as 'Order Id', FK_UserId AS 'User Id', tbl_users.First_Name as 'First Name', tbl_users.Second_Name as 'Second Name', FK_SupplierId as 'Supplier Id', tbl_suppliers.Name as 'Supplier Name', CAST(sum(Total_Cost) as DECimal(10,2)) as Order_total, DateOrdered from ( SELECT PK_OrderId, Total_Cost, FK_UserId, FK_SupplierId, DateOrdered, FK_VerifierId From tbl_orders INNER JOIN tbl_purchase_orders on tbl_orders.FK_OrderId = tbl_purchase_orders.PK_OrderId ) t  INNER JOIN tbl_users on t.FK_UserId = tbl_users.PK_UserId INNER JOIN tbl_suppliers on t.FK_SupplierId = tbl_suppliers.PK_SupplierId WHERE FK_VerifierId is Null OR FK_VerifierId = 0;");
+
+                        if (approveOrders.Rows.Count == 0 ||
+                            approveOrders.Rows[0][0] is System.DBNull)
+                        {
+                            break;
+                        }
+
+                        index = -1;
+
+                        // Find the index of the current user
+                        for (int i = 0; i < VerifyOrders.Count; i++)
+                        {
+                            if (check.UserId == VerifyOrders[i].UserId)
+                            {
+                                index = i;
+                                break;
+                            }
+                        }
+
+                        // Copy the orders
+                        var sendOrders = approveOrders;
+
+                        // If the user is has no previous notifications
+                        if (index != -1)
+                        {
+                            for (int i = 0; i < approveOrders.Rows.Count; i++)
+                            {
+                                if (VerifyOrders[index].ProductId.Contains((int)approveOrders.Rows[i][0]))
+                                {
+                                    sendOrders.Rows.Remove(approveOrders.Rows[i]);
+                                }
+                            }
+                        }
+                        else if (sendOrders.Rows.Count != 0)
+                        {
+                            // Add the user to add the notifications
+                            VerifyOrders.Add(new ProductNotification());
+                            VerifyOrders[VerifyOrders.Count - 1].UserId = check.UserId;
+                            index = VerifyOrders.Count - 1;
+                        }
+
+                        // Add the products to the previous notifications
+                        for (int i = 0; i < sendOrders.Rows.Count; i++)
+                        {
+                            VerifyOrders[index].ProductId.Add((int)sendOrders.Rows[i][0]);
+                        }
+
+                        // If there are still products to send back to the client then send
+                        if (sendOrders.Rows.Count != 0)
+                        {
+                            // Send the orders back to the client
+                            var table = new Table(sendOrders, Program.MachineId, Program.UserId, 2006);
+                            clientSocket.Send(table.Data);
+                            break;
+                        }
                     }
                     clientSocket.Send(new StdData("", Program.MachineId, Program.UserId, 2004).Data);
                     break;
